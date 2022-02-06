354d6eb4398b6dd348c7220563d5d2e832d4a6fc
guswns1659 <guswns1659@gmail.com>
2022-02-07 07:31:11 +0900

    react feat: Add initHeart() when form submiited

--- resources/utils.js
+++ resources/utils.js
@@ -8,7 +8,7 @@ const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
 const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
 
 const jsonLocalStorage = {
-  setItem:/**/ (key, value) => {
+  setItem: (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
   },
   getItem: (key) => {
